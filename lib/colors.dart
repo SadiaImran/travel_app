a69d59bd03da987ab873bedac435ade372328b32
Sadia Imran <sadiaimran837@gmail.com>
2024-11-23 16:47:14 +0500

    cleaned project added fonts & images folder & colors file

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color splashPrimaryBlue = Color(0xFF0D6EFD); // Primary color
+  static const Color lightBackground = Color(0xFFF3F4F6); // Background color
+  static const Color darkText = Color(0xFF212121); // Dark text color
+  static const Color lightText = Color(0xFF757575); // Light text color
+  static const Color accentOrange = Color(0xFFFFA726); // Accent color
+}

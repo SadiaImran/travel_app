9235b2c78bce85fbf29394d9ba246c731b6437fd
Wajiha <wajihawaqar1234@gmail.com>
2024-11-28 19:10:30 +0500

    Home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/screens/home_screen.dart';
 import 'package:travel_app/screens/splash_screen.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Travenor',
-      home: SplashScreen(),
+      home: HomeScreen(),
     );
   }
 }

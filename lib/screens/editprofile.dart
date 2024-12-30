0dec73147d607b99703d1597d21a16bbf428105a
Sadia Imran <sadiaimran837@gmail.com>
2024-12-30 11:17:00 +0500

    edit profile screen updated

--- lib/screens/editprofile.dart
+++ lib/screens/editprofile.dart
@@ -3,8 +3,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class EditProfileScreen extends StatefulWidget {
-  const EditProfileScreen({Key? key}) : super(key: key);
-
+  const EditProfileScreen({Key? key , required this.username}) : super(key: key);
+  final String username;
   @override
   State<EditProfileScreen> createState() => _EditProfileScreenState();
 }
